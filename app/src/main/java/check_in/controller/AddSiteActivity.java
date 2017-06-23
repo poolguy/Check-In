c7d6772ef28a29e7755a0dcce0a21ae23078e5bb
Seth Donaldson <sdonaldson@seths-mac-mini.dnsdhcp.provo.novell.com>
2017-06-23 14:08:49 -0600

    Editing AddSiteActivity

--- app/src/main/java/check_in/controller/AddSiteActivity.java
+++ app/src/main/java/check_in/controller/AddSiteActivity.java
@@ -1,20 +1,48 @@
 package check_in.controller;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.macbookpro.check_in.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import super_class.CheckInMapFragment;
 
-public class AddSiteActivity extends AppCompatActivity {
+public class AddSiteActivity extends AppCompatActivity implements OnMapReadyCallback {
+	private GoogleMap googleMap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_site);
 
-		getFragmentManager().beginTransaction().add(R.id.mapFrameLayout, CheckInMapFragment.newInstance()).commit();
+//		getFragmentManager().beginTransaction().add(R.id.mapFrameLayout, CheckInMapFragment.newInstance()).commit();
 
+		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+		mapFragment.getMapAsync(this);
+
+	}
+
+	@Override
+	public void onMapReady(GoogleMap googleMap) {
+		this.googleMap = googleMap;
+		if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			return;
+		}
+		googleMap.setMyLocationEnabled(true);
+		googleMap.getMyLocation();
 	}
 }
--- app/src/main/java/check_in/controller/MainActivity.java
+++ app/src/main/java/check_in/controller/MainActivity.java
@@ -1,7 +1,9 @@
 package check_in.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.macbookpro.check_in.R;
 
@@ -14,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(MainActivity.this, AddSiteActivity.class);
+				startActivity(intent);
+			}
+		});
 
 
 	}
